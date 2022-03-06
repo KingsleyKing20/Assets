7e4290a4e87fbcb0388d1a5231bc52bc7aebbfd1
Kingsley King <99085652+KingsleyKing20@users.noreply.github.com>
2022-03-06 16:28:27 +0800

    Update bilibili_diy.js

--- QuantumultX/bilibili_diy.js
+++ QuantumultX/bilibili_diy.js
@@ -138,6 +138,28 @@ if (magicJS.read(blackKey)) {
                 e["title"] = "会员购";
                 e["uri"] = "bilibili://mall/home";
             });
+      "title": "创作中心",
+      "items": [
+        {
+          "need_login": 1,
+          "display": 1,
+          "id": 171,
+          "title": "创作首页",
+          "global_red_dot": 1,
+          "uri": "bilibili://uper/homevc",
+          "icon": "http://i0.hdslb.com/bfs/archive/d3aad2d07538d2d43805f1fa14a412d7a45cc861.png"
+        },
+      ],
+      "style": 1,
+      "button": {
+        "icon": "http://i0.hdslb.com/bfs/archive/205f47675eaaca7912111e0e9b1ac94cb985901f.png",
+        "style": 1,
+        "url": "bilibili://uper/user_center/archive_selection",
+        "text": "发布"
+      }
+    },
+    
+    {
       // 直播去广告
       case /^https?:\/\/api\.live\.bilibili\.com\/xlive\/app-room\/v1\/index\/getInfoByRoom/.test(magicJS.request.url):
         try {
