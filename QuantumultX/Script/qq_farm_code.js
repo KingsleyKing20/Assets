7a50afcb59b896244f334c1691a6628aebab4d92
妇科圣手 <99085652+KingsleyKing20@users.noreply.github.com>
2026-03-12 01:08:07 +0800

    Update qq_farm_code.js

--- QuantumultX/Script/qq_farm_code.js
+++ QuantumultX/Script/qq_farm_code.js
@@ -28,9 +28,5 @@ if (url.includes("code=")) {
         $notification.post("QQ农场 Code", "", "已复制: " + code);
         
         // 注意：如果不发送请求，游戏登录会失败。
-        // 如果想让游戏正常运行仅复制 code，请注释掉下面这行
-     #   // $done({response: {status: 404}}); 
-    }
-}
-
+      
 $done({});
